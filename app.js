a4c90cb1e2fe4825428bc694aa447d2b3064a4d4
ivanrozak <ivanrozack@gmail.com>
2020-12-14 08:54:31 +0700

    add CORS and .env

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 // =====================
 const routesNavigation = require('./src/routesNavigation')
 // =====================
@@ -8,7 +9,15 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(cors())
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Request-With, Content-Type, Accept, Authorization'
+  )
+  next()
+})
 app.use('/', routesNavigation)
 
 app.get('*', (request, response) => {
--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql')
+require('dotenv').config()
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: process.env.host,
+  user: process.env.user,
   password: '',
   database: 'db_coffe',
   timezone: 'UTC'
